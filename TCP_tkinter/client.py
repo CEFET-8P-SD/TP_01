d81827c7b3c8879eea02a8acef7534fd3905b91c
Maycon Arthuso de Carvalho <maycon.arth.carvalho@gmail.com>
2021-02-07 12:20:59 -0300

    ajustando o tkinter TCP client

--- TCP_tkinter/client.py
+++ TCP_tkinter/client.py
@@ -7,7 +7,7 @@ import tkinter
 def receber_mensagem():
     while True:
         try:
-            msg = client_socket.recv(BUFSIZ).decode("utf8")
+            msg = client_socket.recv(BUFSIZ).decode('utf8')
             msg_list.insert(tkinter.END, msg)
         except OSError:
             break
@@ -16,41 +16,43 @@ def receber_mensagem():
 # enviar mensagens
 def enviar_mensagem(event=None):
     msg = my_msg.get()
-    my_msg.set("")  # limpar campo de escrita.
-    client_socket.send(bytes(msg, "utf8"))
-    if msg == "{quit}":
+    my_msg.set('')  # limpar campo de escrita.
+    client_socket.send(bytes(msg, 'utf8'))
+    if msg == '{quit}':
         client_socket.close()
         top.quit()
 
 
 # fechar a janela de chat
 def fechar_chat(event=None):
-    my_msg.set("{quit}")
+    my_msg.set('{quit}')
     enviar_mensagem()
 
 
 top = tkinter.Tk()
-top.title("Chat usando Protocolo TCP")
+top.title('Chat usando Protocolo TCP')
+top.resizable(width=True, height=True)
 
 messages_frame = tkinter.Frame(top)
 my_msg = tkinter.StringVar()
-my_msg.set("Escreva seu nome aqui")
+my_msg.set('')
 
 scrollbar = tkinter.Scrollbar(messages_frame)
-msg_list = tkinter.Listbox(messages_frame, height=25, width=70, yscrollcommand=scrollbar.set)
 scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
-msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
 
-msg_list.pack()
+msg_list = tkinter.Listbox(messages_frame, height=25, width=70, yscrollcommand=scrollbar.set)
+msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
 messages_frame.pack()
 
 entry_field = tkinter.Entry(top, textvariable=my_msg, width=65)
-entry_field.bind("<Return>", enviar_mensagem)
+entry_field.bind('<Return>', enviar_mensagem)
 entry_field.pack()
-send_button = tkinter.Button(top, text="Enviar", command=enviar_mensagem)
+entry_field.focus()
+
+send_button = tkinter.Button(top, text='Enviar', font='Helvetica 10 bold', width=20, command=enviar_mensagem)
 send_button.pack()
 
-top.protocol("WM_DELETE_WINDOW", fechar_chat)
+top.protocol('WM_DELETE_WINDOW', fechar_chat)
 
 HOST = '127.0.0.1'
 PORT = 33001
