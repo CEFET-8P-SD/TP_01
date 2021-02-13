bf0f563d2a76aeb35a2427912bb3241779e7fa63
Maycon Arthuso de Carvalho <maycon.arth.carvalho@gmail.com>
2021-02-13 14:05:27 -0300

    limpando codigo

--- UDP_tkinter/client.py
+++ UDP_tkinter/client.py
@@ -1,35 +1,34 @@
 from socket import socket, AF_INET, SOCK_DGRAM
 import tkinter
-import time
+from threading import Thread
 
 cliente = socket(family=AF_INET, type=SOCK_DGRAM)
 
-#---------------------------------------------------------
+# ---------------------------------------------------------
 #   Definindo o endereço de destino - conjunto (ip, porta)
-#---------------------------------------------------------
+# ---------------------------------------------------------
 host = '127.0.0.1'
 port = 5000
 BUFSIZ = 1024
 
-destino = (host,port)
+destino = (host, port)
 
-#---------------------------------------------------------
+# ---------------------------------------------------------
 #   Iniciando variável de Primeira conexão
-#---------------------------------------------------------
+# ---------------------------------------------------------
 envio_1 = False
 
 
 def primeira_conexao():
-    
     input_msg = ""
     cliente.sendto(input_msg.encode(), destino)
     envio_1 = True
 
-#---------------------------------------------------------
-#   Configurando métodos de envia e chegada
-#---------------------------------------------------------
-def receber_mensagem():
 
+# ---------------------------------------------------------
+#   Configurando métodos de envia e chegada
+# ---------------------------------------------------------
+def receber_mensagem():
     while True:
         try:
             msg = cliente.recv(BUFSIZ).decode('utf8')
@@ -39,27 +38,25 @@ def receber_mensagem():
 
 
 def enviar_mensagem(event=None):
-
     input_msg = my_msg.get()
     my_msg.set('')
     cliente.sendto(input_msg.encode(), destino)
 
-    if input_msg == '{quit}': 
-
+    if input_msg == '{quit}':
         cliente.close()
         top.quit()
 
-def fechar_chat(event=None):
 
+def fechar_chat(event=None):
     my_msg.set('{quit}')
-    enviar_mensagem()    
+    enviar_mensagem()
 
 
 primeira_conexao()
 
-#---------------------------------------------------------
+# ---------------------------------------------------------
 #   Configurando Interface de comunicação com o cliente
-#---------------------------------------------------------
+# ---------------------------------------------------------
 top = tkinter.Tk()
 top.title('Protocolo UDP')
 top.resizable(width=True, height=True)
@@ -85,7 +82,6 @@ send_button.pack()
 
 top.protocol('WM_DELETE_WINDOW', fechar_chat)
 
-from threading import Thread
 receive_thread = Thread(target=receber_mensagem)
 receive_thread.start()
 tkinter.mainloop()
